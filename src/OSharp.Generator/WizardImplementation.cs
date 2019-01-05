72fc67c4560c5aee106adc2975fd15b2f4b185f9
郭明锋 <gmf520@yeah.net>
2019-01-05 20:58:53 +0800

    添加实体项工程

---
+++ src/OSharp.Generator/WizardImplementation.cs
@@ -0,0 +1,34 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+
+namespace OSharp.Generator
+{
+    public class WizardImplementation : IWizard
+    {
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind,
+            object[] customParams)
+        {
+            
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        { }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        { }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        { }
+
+        public void RunFinished()
+        { }
+    }
+}
